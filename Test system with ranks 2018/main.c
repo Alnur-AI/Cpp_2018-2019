d718a99a499dcd1d5283b0b3ccf1b8ef495e4469
logiteg00 <65267507+logiteg00@users.noreply.github.com>
2021-05-25 21:18:20 +0300

    Add files via upload

---
+++ Test system with ranks 2018/main.c
@@ -0,0 +1,338 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+
+const int A = 1;
+const int B = 2;
+const int C = 3;
+const int D = 4;
+const int E = 5;
+
+FILE *answers;
+FILE *questions;
+FILE *results;
+
+
+int calculatingHowMuchQuestionsTotal(){
+    int questionCounterTotal = 0;
+    answers = fopen("answers.txt","r");
+    char buf[1024];
+    for (int i = 0; ;i++)
+    {
+        fgets(buf,1024,answers);
+        if (buf[0]=='-')
+        {
+            fclose(answers);
+            return questionCounterTotal;
+        }
+        questionCounterTotal++;
+    }
+}
+int calculatingHowMuchQuestionsTotalRank_A(){
+    questions = fopen("test.txt","r");
+    char buffer [1024];
+    int questionCounterRANK_A = 0;
+    for (int i = 0; ;i++)
+    {
+        fgets(buffer,1024,questions);
+        if (buffer[0] == 'R' && buffer[1] == 'A' && buffer[2] == 'N' && buffer[3] == 'K' && buffer[4] == ' ' && buffer[5] == 'A')
+        {
+            questionCounterRANK_A++;
+        }
+        else if (buffer[0] == '-' && buffer[1] == '-' && buffer[2] == '-' && buffer[3] == '-' && buffer[4] == '-')
+        {
+            fclose(questions);
+            return questionCounterRANK_A;
+        }
+    }
+}
+int calculatingHowMuchQuestionsTotalRank_B(){
+    questions = fopen("test.txt","r");
+    char buffer [1024];
+    int questionCounterRANK_B = 0;
+    for (int i = 0; ;i++)
+    {
+        fgets(buffer,1024,questions);
+        if (buffer[0] == 'R' && buffer[1] == 'A' && buffer[2] == 'N' && buffer[3] == 'K' && buffer[4] == ' ' && buffer[5] == 'B')
+        {
+            questionCounterRANK_B++;
+        }
+        else if (buffer[0] == '-' && buffer[1] == '-' && buffer[2] == '-' && buffer[3] == '-' && buffer[4] == '-')
+        {
+            fclose(questions);
+            return questionCounterRANK_B;
+        }
+    }
+}
+int calculatingHowMuchQuestionsTotalRank_C(){
+    questions = fopen("test.txt","r");
+    char buffer [1024];
+    int questionCounterRANK_C = 0;
+    for (int i = 0; ;i++)
+    {
+        fgets(buffer,1024,questions);
+        if (buffer[0] == 'R' && buffer[1] == 'A' && buffer[2] == 'N' && buffer[3] == 'K' && buffer[4] == ' ' && buffer[5] == 'C')
+        {
+            questionCounterRANK_C++;
+        }
+        else if (buffer[0] == '-' && buffer[1] == '-' && buffer[2] == '-' && buffer[3] == '-' && buffer[4] == '-')
+        {
+            fclose(questions);
+            return questionCounterRANK_C;
+        }
+    }
+}
+int calculatingHowMuchQuestionsTotalRank_D(){
+    questions = fopen("test.txt","r");
+    char buffer [1024];
+    int questionCounterRANK_D = 0;
+    for (int i = 0; ;i++)
+    {
+        fgets(buffer,1024,questions);
+        if (buffer[0] == 'R' && buffer[1] == 'A' && buffer[2] == 'N' && buffer[3] == 'K' && buffer[4] == ' ' && buffer[5] == 'D')
+        {
+            questionCounterRANK_D++;
+        }
+        else if (buffer[0] == '-' && buffer[1] == '-' && buffer[2] == '-' && buffer[3] == '-' && buffer[4] == '-')
+        {
+            fclose(questions);
+            return questionCounterRANK_D;
+        }
+    }
+}
+int calculatingHowMuchQuestionsTotalRank_E(){
+    questions = fopen("test.txt","r");
+    char buffer [1024];
+    int questionCounterRANK_E = 0;
+    for (int i = 0; ;i++)
+    {
+        fgets(buffer,1024,questions);
+        if (buffer[0] == 'R' && buffer[1] == 'A' && buffer[2] == 'N' && buffer[3] == 'K' && buffer[4] == ' ' && buffer[5] == 'E')
+        {
+            questionCounterRANK_E++;
+        }
+        else if (buffer[0] == '-' && buffer[1] == '-' && buffer[2] == '-' && buffer[3] == '-' && buffer[4] == '-')
+        {
+            fclose(questions);
+            return questionCounterRANK_E;
+        }
+    }
+}
+
+
+void fillCorrectAnswersInSheet(char a[]){
+    char buf[1024];
+    answers = fopen("answers.txt", "r");
+    for (int i = 0; ; i++)
+    {
+        fgets(buf, 1024, answers);
+        if (buf[2] == '-')
+        {
+            fclose(answers);
+            return;
+        }
+        a[i] = buf[2];
+    }
+}
+void makeArrayEmpty(char array[],int howMuchElements ){
+    for (int i = 0; i < howMuchElements; i++)
+    {
+        array[i] = '*';
+    }
+    return;
+}
+
+int RANKchecker(int Ai, int Bi, int Ci, int Di, int Ei){
+    if (Ai == 0 && Bi == 0 && Ci == 0 && Di == 0 && Ei == 0) return 0;
+    int array[5] = {Ai, Bi, Ci, Di, Ei};
+    for (int i = 0; i < 5; i++)
+    {
+        if (array[i]!=0) return i + 1;
+    }
+}
+
+int main()
+{
+    int currentRANK;
+    char currentAnswer;
+    int currentQuestion = 1;
+    int solvedQuestions = 0;
+
+    int howMuchQuestions       = calculatingHowMuchQuestionsTotal();
+    int howMuchQuestionsRank_A = calculatingHowMuchQuestionsTotalRank_A();
+    int howMuchQuestionsRank_B = calculatingHowMuchQuestionsTotalRank_B();
+    int howMuchQuestionsRank_C = calculatingHowMuchQuestionsTotalRank_C();
+    int howMuchQuestionsRank_D = calculatingHowMuchQuestionsTotalRank_D();
+    int howMuchQuestionsRank_E = calculatingHowMuchQuestionsTotalRank_E();
+
+    int leftQuestionsRANK_A = howMuchQuestionsRank_A;
+    int leftQuestionsRANK_B = howMuchQuestionsRank_B;
+    int leftQuestionsRANK_C = howMuchQuestionsRank_C;
+    int leftQuestionsRANK_D = howMuchQuestionsRank_D;
+    int leftQuestionsRANK_E = howMuchQuestionsRank_E;
+
+    char correctAnswers [howMuchQuestions];
+    char studentAnswers [howMuchQuestions];
+
+    fillCorrectAnswersInSheet(correctAnswers);
+    makeArrayEmpty(studentAnswers, howMuchQuestions);
+
+    //printf("Hello! Let's start the test!\n\n");
+    //printf("%s\n%s\nTotal:%d\n (1)A:%d\n (2)B:%d\n (3)C:%d\n (4)D:%d\n (5)E:%d\n\n", correctAnswers, studentAnswers, howMuchQuestions, leftQuestionsRANK_A, leftQuestionsRANK_B, leftQuestionsRANK_C, leftQuestionsRANK_D, leftQuestionsRANK_E);
+
+    int valueRANKchecker = RANKchecker(leftQuestionsRANK_A, leftQuestionsRANK_B, leftQuestionsRANK_C, leftQuestionsRANK_D, leftQuestionsRANK_E);
+    if (valueRANKchecker == 0)
+    {
+        printf("There are no questions! ");
+        return 0;
+    }
+
+    char buffer[1024];
+    questions = fopen("test.txt","r");
+    fgets(buffer, 1024, questions);
+
+    while (valueRANKchecker != 0){
+
+        system("cls");
+        valueRANKchecker = RANKchecker(leftQuestionsRANK_A, leftQuestionsRANK_B, leftQuestionsRANK_C, leftQuestionsRANK_D, leftQuestionsRANK_E);
+        printf("  Right answers: %s\nStudent answers: %s\n Total:%d\n (1)A:%d\n (2)B:%d\n (3)C:%d\n (4)D:%d\n (5)E:%d\n\n", correctAnswers, studentAnswers, howMuchQuestions, leftQuestionsRANK_A, leftQuestionsRANK_B, leftQuestionsRANK_C, leftQuestionsRANK_D, leftQuestionsRANK_E);
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),6);
+        printf("Your current rank: %d\n\n", valueRANKchecker );
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),15);
+
+
+        while (buffer[0] != 'R' && buffer[1] != 'A' && buffer[2] != 'N' && buffer[3] != 'K'){
+            fgets(buffer, 1024, questions);
+            //printf("%s",buffer);
+            if (buffer[0] == '-' && buffer[1] == '-' && buffer[2] == '-' ){
+                fclose(questions);
+                questions = fopen("test.txt","r");
+                currentQuestion = 1;
+            }
+
+        }
+
+
+        if(buffer[0] == 'R' && buffer[1] == 'A' && buffer[2] == 'N' && buffer[3] == 'K' && buffer [5] == 'A' && valueRANKchecker == A){
+
+
+
+                while (buffer[0] != '|' && buffer[1] != '|' && buffer[2] != '|' ){
+                    fgets(buffer, 1024, questions);
+                    printf("%s",buffer);
+                }
+
+                printf("\nMy answer is: ");
+                currentAnswer = getchar();
+                //scanf("%c",&currentAnswer);
+                studentAnswers[currentQuestion - 1] = currentAnswer;
+                currentQuestion++;
+                solvedQuestions++;
+                leftQuestionsRANK_A--;
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+                printf(":::::ITERATION COMPLITED:::::\n");
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),15);
+                continue;
+        }
+        if(buffer[0] == 'R' && buffer[1] == 'A' && buffer[2] == 'N' && buffer[3] == 'K' && buffer [5] == 'B' && valueRANKchecker == B){
+
+
+                while (buffer[0] != '|' && buffer[1] != '|' && buffer[2] != '|' ){
+                    fgets(buffer, 1024, questions);
+                    printf("%s",buffer);
+                }
+                printf("\nMy answer is: ");
+                currentAnswer = getchar();
+                //scanf("%c",&currentAnswer);
+                studentAnswers[currentQuestion - 1] = currentAnswer;
+                currentQuestion++;
+                solvedQuestions++;
+                leftQuestionsRANK_B--;
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+                printf(":::::ITERATION COMPLITED:::::\n");
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),15);
+                continue;
+        }
+        if(buffer[0] == 'R' && buffer[1] == 'A' && buffer[2] == 'N' && buffer[3] == 'K' && buffer [5] == 'C' && valueRANKchecker == C){
+
+
+                while (buffer[0] != '|' && buffer[1] != '|' && buffer[2] != '|' ){
+                    fgets(buffer, 1024, questions);
+                    printf("%s",buffer);
+                }
+
+                printf("\nMy answer is: ");
+                currentAnswer = getchar();
+                //scanf("%c",&currentAnswer);
+                studentAnswers[currentQuestion - 1] = currentAnswer;
+                currentQuestion++;
+                solvedQuestions++;
+                leftQuestionsRANK_C--;
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+                printf(":::::ITERATION COMPLITED:::::\n");
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),15);
+                continue;
+        }
+        if(buffer[0] == 'R' && buffer[1] == 'A' && buffer[2] == 'N' && buffer[3] == 'K' && buffer [5] == 'D' && valueRANKchecker == D){
+
+
+                while (buffer[0] != '|' && buffer[1] != '|' && buffer[2] != '|' ){
+                    fgets(buffer, 1024, questions);
+                    printf("%s",buffer);
+                }
+
+                printf("\nMy answer is: ");
+                currentAnswer = getchar();
+                //scanf("%c",&currentAnswer);
+                studentAnswers[currentQuestion - 1] = currentAnswer;
+                currentQuestion++;
+                solvedQuestions++;
+                leftQuestionsRANK_D--;
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+                printf(":::::ITERATION COMPLITED:::::\n");
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),15);
+                continue;
+        }
+        if(buffer[0] == 'R' && buffer[1] == 'A' && buffer[2] == 'N' && buffer[3] == 'K' && buffer [5] == 'E' && valueRANKchecker == E){
+
+
+                while (buffer[0] != '|' && buffer[1] != '|' && buffer[2] != '|' ){
+                    fgets(buffer, 1024, questions);
+                    printf("%s",buffer);
+                }
+
+                printf("\nMy answer is: ");
+                currentAnswer = getchar();
+                //scanf("%c",&currentAnswer);
+                studentAnswers[currentQuestion - 1] = currentAnswer;
+                currentQuestion++;
+                solvedQuestions++;
+                leftQuestionsRANK_E--;
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+                printf(":::::ITERATION COMPLITED:::::\n");
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),15);
+                continue;
+        }
+
+        fgets(buffer, 1024, questions);
+        while (buffer[0] != 'R' && buffer[1] != 'A' && buffer[2] != 'N' && buffer[3] != 'K'){
+            fgets(buffer, 1024, questions);
+            //printf("%s",buffer);
+            if (buffer[0] == '-' && buffer[1] == '-' && buffer[2] == '-' ){
+                fclose(questions);
+                questions = fopen("test.txt","r");
+                currentQuestion = 1;
+            }
+        }
+        currentQuestion++;
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+        printf(":::::ITERATION COMPLITED:::::\n");
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),15);
+    }
+
+
+
+    fclose(questions);
+    return 0;
+}
